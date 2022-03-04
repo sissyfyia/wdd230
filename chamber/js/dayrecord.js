d4a84b36d874b8faba8694ebbdc0c6321668350a
sissyfyia <asmithmt@gmail.com>
2022-03-03 19:10:41 -0800

    Lessonrework

--- chamber/js/dayrecord.js
+++ chamber/js/dayrecord.js
@@ -5,13 +5,15 @@ let millisecondsToDays = 86400000;
 let prior = localStorage.getItem('lastvisit');
 let obj = Date.now(prior);
 
-if (prior != '') {
+if (!prior) {
     visit = ((today - obj) / millisecondsToDays).toFixed(0);
 }else {
     visit = today + " This is your first visit. Welcome!"
 }
 
-document.getElementById("lastv").innerHTML = visit + " days ago. Welcome!";
-localStorage.setItem('lastvisit', today);
+document.getElementById("lastVisit").innerHTML += visit + " days ago. Welcome!";
+localStorage.setItem("lastVisit", today);
+
+document.querySelector(".lastVisit");
+
 }
-return lastVisit();
