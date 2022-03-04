a05993f1e75066c68fe8ab31cab7f3cccb124847
sissyfyia <asmithmt@gmail.com>
2022-03-03 22:26:50 -0800

    finalonprophets

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -20,13 +20,11 @@ fetch(requestURL)
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
-    let h3 = document.createElement('h3');
     let h4 = document.createElement('h4');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-    h3.innerHTML =`${prophet.birthdate}`;
-    h4.innerHTML =`${prophet.birthplace}`;
+    h4.innerHTML =`<span>Birthplace: ${prophet.birthdate} </span><br><span> Birthdate: ${prophet.birthplace}</span>`;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
@@ -35,8 +33,8 @@ fetch(requestURL)
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(h4);
     card.appendChild(portrait);
-    card.appendChild();
   
     // Add/append the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
