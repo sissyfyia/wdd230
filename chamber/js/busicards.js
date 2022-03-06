92652439cfbfb3b357debc6a41470b110724e68b
sissyfyia <asmithmt@gmail.com>
2022-03-05 21:54:46 -0800

    trial2

--- chamber/js/busicards.js
+++ chamber/js/busicards.js
@@ -1,6 +1,6 @@
 
-const requestURL ='https://sissyfyiawdd30.github.io/chamber/data/data.json';
-const cards = document.querySelector('.bcards');
+const requestURL ='https://sissyfyia.github.io/wdd230/chamber/data/data.json';
+const busicards = document.querySelector('.busicards');
 
 fetch(requestURL)
   .then(function (response){
@@ -11,11 +11,11 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
-    const cards = jsonObject['busicards'];
-    prophets.forEach(displayCards);  // temporary checking for valid response and data parsing
+    const bcards = jsonObject['bcards'];
+    bcards.forEach(displayCards);  // temporary checking for valid response and data parsing
   });
   
-  function displayCards(prophet) {
+  function displayCards(bcards) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
@@ -28,14 +28,14 @@ fetch(requestURL)
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     logo.setAttribute('src', bcards.imageurl);
-    //logo.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} ${prophet.order} Latter-day President: ${prophet.order}`);//
+    logo.setAttribute('alt', `${bcards.name}`);//
     logo.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
-    bcards.appendChild(h2);
-    bcards.appendChild(h4);
-    bcards.appendChild(portrait);
+    card.appendChild(h2);
+    card.appendChild(h4);
+    //bcards.appendChild(logo);//
   
     // Add/append the existing HTML div with the cards class with the section(card)
-    bcards.appendChild(bcards);
+    card.appendChild(busicards);
   }
