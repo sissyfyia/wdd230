d4a8859eafaa6c961d7e1476a83eb3b19df90367
sissyfyia <asmithmt@gmail.com>
2022-03-16 22:42:38 -0600

    commit9

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -3,7 +3,7 @@ const apiURL= 'https://api.openweathermap.org/data/2.5/weather?lat=37&lon=-105&u
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    //console.log(jsObject);
+  console.log(jsObject);
     
 const currentTemp = document.querySelector('#current-temp');
 const weathericon = document.querySelector('#icon-src');
@@ -20,27 +20,27 @@ document.querySelector('#weathericon').setAttribute('src', iconsrc);
 document.querySelector('#weathericon').setAttribute('alt', desc);
 document.querySelector('figcaption').textContent = desc;
 
-let temperature = currentTemp;
 const speed = document.querySelector('#speed');
 const windChill = document.querySelector("#windChill");
 
 speed.textContent = jsObject.wind.speed.toFixed(0);
-currentTemp.textContent = jsObject.main.temp.toFixed(0);
+windChill.textContent= jsObject.main.feels_like.toFixed(0);
+//currentTemp.textContent = jsObject.main.temp.toFixed(0);
 
-function getChill(temperature, speed) {
+//function getChill(temperature, speed) {
 
-    let chill= 35.74 + 0.6215 * temperature - 35.75 * (speed ** 0.16) + 0.46275 * temperature * (speed ** 0.16);
-    chill = Math.floor(chill);
-    console.log(chill);
+    //let chill= 35.74 + 0.6215 * temperature - 35.75 * (speed ** 0.16) + 0.46275 * temperature * (speed ** 0.16);
+    //chill = Math.floor(chill);
+    //console.log(chill);
 
     //return chill;
   
-}
+//}
 
-if (temperature <= 50 && speed >= 3) {
-    let finalChill = getChill(temperature, speed);
-    windChill.innerHTML = finalChill;
-} else {
-    let windChill = 'N/A';
-}
+//if (temperature <= 50 && speed.innerHTML >= 3) {
+  //  let finalChill = getChill(temperature, speed);
+  //  windChill.innerHTML = finalChill;
+//} else {
+//    let windChill = 'N/A';
+//}
 });
