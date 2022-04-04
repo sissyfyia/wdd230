0763f60f966f3c52bfaabb0a4a59effd4dd3f4b0
Use your GitHub username <Enter your email address>
2022-04-04 15:33:41 -0600

    commit8

--- TempleInn/js/weatherapi.js
+++ TempleInn/js/weatherapi.js
@@ -1,8 +1,8 @@
 
 const apiURL= 'https://api.openweathermap.org/data/2.5/weather?lat=39&lon=-104&units=imperial&appid=66df575ecb5b3b5511f996e4aaa99495'
 
-const currentTemp = document.querySelector('#current-temp');
-const weathericon = document.querySelector('#icon-src');
+const currentTemp = document.querySelector('#currenttemp');
+const weathericon = document.querySelector('#iconsrc');
 const caption = document.querySelector('figcaption');
 
 fetch(apiURL)
@@ -17,7 +17,7 @@ let imgalt = jsObject.weather[0].description;
 
 const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;
-//document.querySelector('#icon-src').textContent = iconsrc;
+document.querySelector('#iconsrc').textContent = iconsrc;
 document.querySelector('#weathericon').setAttribute('src', iconsrc);
 document.querySelector('#weathericon').setAttribute('alt', desc);
 document.querySelector('figcaption').textContent = desc;
