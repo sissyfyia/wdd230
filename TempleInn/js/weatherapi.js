405c61a5a1f18957450c79b7a07a67ea92bb5d53
Use your GitHub username <Enter your email address>
2022-04-04 20:37:53 -0600

    commit9

--- TempleInn/js/weatherapi.js
+++ TempleInn/js/weatherapi.js
@@ -1,26 +1,25 @@
 
-const apiURL= 'https://api.openweathermap.org/data/2.5/weather?lat=39&lon=-104&units=imperial&appid=66df575ecb5b3b5511f996e4aaa99495'
+const apiURL= `https://api.openweathermap.org/data/2.5/onecall?lat=39&lon=-105&exclude=minutely&units=imperial&appid=66df575ecb5b3b5511f996e4aaa99495`;
 
-const currentTemp = document.querySelector('#currenttemp');
-const weathericon = document.querySelector('#iconsrc');
-const caption = document.querySelector('figcaption');
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
   console.log(jsObject);
 
+  const currentTemp = document.querySelector('#current-temp');
+  const weathericon = document.querySelector('#weathericon');
+  const humid = document.querySelector('#humidity');
+  const desc = document.querySelector('#desc');
 
-currentTemp.textContent = jsObject.main.temp.toFixed(0);
-let imgsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-let imgalt = jsObject.weather[0].description;
+  currentTemp.textContent = jsObject.current.temp.toFixed(0);
+ // let imgsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+ // let imgalt = jsObject.weather[0].description;
 
-const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-const desc = jsObject.weather[0].description;
-document.querySelector('#iconsrc').textContent = iconsrc;
-document.querySelector('#weathericon').setAttribute('src', iconsrc);
-document.querySelector('#weathericon').setAttribute('alt', desc);
-document.querySelector('figcaption').textContent = desc;
+  currentTemp.textContent = jsObject.current.temp.toFixed(0);
+ // weathericon.setAttribute('src', imgsrc);
+  //weathericon.setAttribute('alt', imgalt);
+  desc.textContent = jsObject.current.weather[0].description;
+  humid.textContent = jsObject.current.humidity.toFixed(0);
 
-
-  });
+  })
