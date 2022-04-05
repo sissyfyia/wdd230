8860d1afd6c1452239a6fba2f5cc2432d1038409
Use your GitHub username <Enter your email address>
2022-04-04 23:12:45 -0600

    commit10

--- TempleInn/js/toggle.js
+++ TempleInn/js/toggle.js
@@ -3,5 +3,5 @@ function toggleMenu() {
     document.getElementById("dropBtn").classList.toggle("open");
 }
 
-const x = document.getElementById("dropBtn");
+const x = document.getElementById('dropBtn');
 x.onclick = toggleMenu;
