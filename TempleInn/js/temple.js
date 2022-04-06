1d609690129108ccaf17fdc824794dd93d9b0bc7
Use your GitHub username <Enter your email address>
2022-04-05 22:39:34 -0600

    commit15

--- TempleInn/js/temple.js
+++ TempleInn/js/temple.js
@@ -18,15 +18,18 @@ fetch(requestURL)
     let card = document.createElement('section');
     let h1 = document.createElement('h1');
     let h2 = document.createElement('h2');
-    let p = document.createElement('p');
+    let p1 = document.createElement('p1');
+    let p2 = document.createElement('p2');
 
     h1.innerHTML = `${info.name}`;
     h2.innerHTML = `Address: ${info.address} <br> Phone: ${info.phone} <br> ${info.email}`;
-    p.innerHTML = `${info.announced}. <br> ${info.gb} <br> ${info.dedic} <br> The closing dates are ${info.closed}. <br> Sessions are ${info.sessions} and services available are ${info.services}`;
+    p1.innerHTML = `${info.announced}. <br> ${info.gb} <br> ${info.dedic} <br> The closing dates are ${info.closed}. <br> Sessions are ${info.sessions}`;
+    p2.innerHTML = `${info.services}`;
 
     card.appendChild(h1);
     card.appendChild(h2);
-    card.appendChild(p);
+    card.appendChild(p1);
+    card.appendChild(p2)
 
     templego.appendChild(card); 
   }
